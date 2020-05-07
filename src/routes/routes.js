3273750464f2bbc46feff8cdb1a641bdb671d218
Kwangyoung <kwangyoung.kim@gmail.com>
2020-05-07 16:41:37 +0900

    Completed module

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,8 +1,10 @@
-import { addnewProduct, getProducts, getProductWithID } from '../controllers/controllers';
+import { addnewProduct, getProducts, getProductWithID, updateProduct, deleteProduct } from '../controllers/controllers';
 
 const routes = app => {
   app.route('/products/:ProductID')
-    .get(getProductWithID);
+    .get(getProductWithID)
+    .put(updateProduct)
+    .delete(deleteProduct);
   app.route('/products')
     .get(getProducts)
     .post(addnewProduct);
