416a4b4d19926879153867e82c43ddf20acc7102
Kwangyoung <kwangyoung.kim@gmail.com>
2020-05-07 15:22:36 +0900

    Completed module

--- index.js
+++ index.js
@@ -1,18 +1,21 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
+import routes from './src/routes/routes';
 
 const app = express();
 const PORT = 4000;
-
-// bodyparser setup
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
 
 mongoose.Promise = global.Promise;
 mongoose.connect('mongodb://localhost/productsdb', { useNewUrlParser: true, useUnifiedTopology: true })
     .then(console.log(`===> Succeeded in connecting to mongodb`))
     .catch((err) => console.log(`===> Error connecting to mongodb - ${err}`));
+
+// bodyparser setup
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+routes(app);
 
 app.get('/', (req, res) =>
     res.send(`Store server running on port ${PORT}`)
